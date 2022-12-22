1f7c624183a292a410c8f248ff5638797201fedb
Yuliya Kurilchik <hidrainn@gmail.com>
2022-12-22 18:05:14 +0100

    Stiled a header block

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,22 @@
-import React from 'react'
-
 type Props = {
     score: number
 }
 
 export const Header = ({ score }: Props) => {
     return (
-        <div>
-            <h1>Score: {score}</h1>
+        <div className="header">
+            <div className="text">
+                <span>Rock</span>
+                <span>Paper</span>
+                <span>Scissors</span>
+            </div>
+
+            <div className="score-box">
+                <span>Score</span>
+                <div className="score-box__score">
+                    {score}
+                </div>
+            </div>
         </div>
     )
 }
