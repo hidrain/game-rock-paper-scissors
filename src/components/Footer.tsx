3c5523606edbe7c8bfb02cc313aa663cebd8aed2
Yuliya Kurilchik <hidrainn@gmail.com>
2022-12-28 14:54:35 +0100

    Footer was styled

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Modal } from './Modal'
 
 type Props = {}
 
 export const Footer = (props: Props) => {
+
+    const [modal, setModal] = useState(false)
+
+    const toggle = () => {
+        setModal(!modal)
+    }
+
     return (
-        <div>Footer</div>
+        <>
+            <footer className="footer">
+                <div className="atribution">
+                    Coded by <a href='https://github.com/hidrain'>Yuliya Kurilchik</a>
+                </div>
+
+                <button className="rules">
+                    Rules
+                </button>
+            </footer>
+
+            {
+                modal ?
+                    <Modal toggle={toggle} />
+                    : null
+            }
+        </>
     )
 }
