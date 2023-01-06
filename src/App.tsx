3e70ecb94ca9751784d97fed51755e0878a1766e
Yuliya Kurilchik <hidrainn@gmail.com>
2023-01-06 17:43:30 +0100

    Fixed mobile version

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { Header } from './components/Header'
 import { Routes, Route } from 'react-router-dom'
 import { Play } from './components/Play';
